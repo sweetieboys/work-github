061fdf5af2c06dadbdfa23eb9163434965274a9a
sakamotoyuuya <149666847+sakamotoyuuya@users.noreply.github.com>
2023-12-18 09:07:24 +0000

    顧客、ログイン機能を編集

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -21,7 +21,13 @@ class Admin::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+   def configure_sign_in_params
+     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+   end
+  def after_sign_in_path_for(resource)
+      public_orders_index_path
+  end
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,105 +1,148 @@
 Rails.application.routes.draw do
 
-  #devise_for :customers
-  #devise_for :admins
+# 顧客用
 
-  # 顧客用
-  # URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
+# URL /customers/sign_in ...
 
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
+devise_for :customers,skip: [:passwords], controllers: {
 
-  namespace :admin do
-    get 'order_details/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'costomers/index'
-    get 'costomers/show'
-    get 'costomers/edit'
-    get 'costomers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/thanks'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdraw'
-  end
-  namespace :public do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'registrations/new'
-    get 'registrations/create'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  devise_for :users
+registrations: "public/registrations",
 
-  devise_for :admins
-  devise_for :customers
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+sessions: 'public/sessions'
+
+}
+
+
+
+# 管理者用
+
+# URL /admin/sign_in ...
+
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+
+sessions: "admin/sessions"
+
+}
+
+
+
+namespace :admin do
+
+get 'order_details/update'
+
+end
+
+namespace :admin do
+
+get 'orders/show'
+
+end
+
+namespace :admin do
+
+get 'costomers/index'
+
+get 'costomers/show'
+
+get 'costomers/edit'
+
+get 'costomers/update'
+
+end
+
+namespace :admin do
+
+get 'genres/index'
+
+get 'genres/create'
+
+get 'genres/edit'
+
+get 'genres/update'
+
+end
+
+namespace :admin do
+
+get 'items/index' => 'items#index'
+
+get 'items/new'
+
+get 'items/create'
+
+get 'items/show'
+
+get 'items/edit'
+
+get 'items/update'
+
+end
+
+namespace :admin do
+
+get 'homes/top'
+
+end
+
+namespace :admin do
+
+get 'sessions/new'
+
+get 'sessions/create'
+
+get 'sessions/destroy'
+
+end
+
+
+
+scope module: :public do
+
+root to: 'homes#top'
+
+get '/about' => 'homes#about'
+
+resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+
+resources :orders, only: [:new, :create, :index, :show]
+
+resources :cart_items, only: [:index, :update, :destroy, :create]
+
+resources :items, only: [:index, :show]
+
+resources :sessions, only: [:new, :create, :destroy]
+
+resources :registrations, only: [:new, :create]
+
+resources :customers, only: [:show]
+
+get "customers/show" => "customers#show"
+
+get "customers/information/edit" => "customers#edit"
+
+patch "customers/information" => "customers#update"
+
+get 'customers/unsubscribe' => "customers#unsubscribe"
+
+patch 'customers/withdraw' => "customers#withdraw"
+
+get 'orders/confirm' => 'orders#confirm'
+
+get 'orders/thanks' => 'orders#thanks'
+
+get 'cart_items/destroy_all' => 'cart_items#destroy_all'
+
+end
+
+
+
+
+
+
+
+
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+end
 end
