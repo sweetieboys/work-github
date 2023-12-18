72767fcffa0e34e7d3d089c91589596f35c05e59
hiyohichi <hiyohichi07@gmail.com>
2023-12-18 09:07:14 +0000

    管理者側顧客画面

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,29 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :addresses, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  
+  def full_name
+    last_name + '' + first_name
+  end
+  
+  def full_name_kana
+    last_name_kana + '' + first_name_kana
+  end
+  
+  def customer_status
+    if is_deleted == true
+      "退会"
+    else
+      "有効"
+    end
+  end
+  
+  #退会したユーザーがログインできないようにする
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,7 @@
 class OrderDetail < ApplicationRecord
   belongs_to :orders
   belongs_to :items
+
+  #enumの定義　製造ステータス
+  enum making_status: { fuka: 0, mati: 1, making: 2, complete: 3 }
 end
