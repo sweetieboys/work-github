5f6be7e0489e3747442b25d91148a1b08e735506
yujiOnuki <yujiuniuni@gmail.com>
2023-12-18 09:09:50 +0000

    public top,aboutページ　admin index,newページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   #devise_for :customers
   #devise_for :admins
 
@@ -50,56 +51,29 @@ Rails.application.routes.draw do
     get 'sessions/create'
     get 'sessions/destroy'
   end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/thanks'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdraw'
-  end
-  namespace :public do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'registrations/new'
-    get 'registrations/create'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  devise_for :users
 
-  devise_for :admins
-  devise_for :customers
-  
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :orders, only: [:new, :create, :index, :show]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :items, only: [:index, :show]
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :registrations, only: [:new, :create]
+    resources :customers, only: [:show]
+    get "customers/show" => "customers#show"
+    get "customers/information/edit" => "customers#edit"
+    patch "customers/information" => "customers#update"
+    get 'customers/unsubscribe' => "customers#unsubscribe"
+    patch 'customers/withdraw' => "customers#withdraw"
+    get 'orders/confirm' => 'orders#confirm'
+    get 'orders/thanks' => 'orders#thanks'
+    get 'cart_items/destroy_all' => 'cart_items#destroy_all'
+  end
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
