713479ccbd25be4729de378c97dafd4b7a0d32a6
hiyohichi <hiyohichi07@gmail.com>
2023-12-21 07:58:20 +0000

    配送先画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,17 +1,42 @@
 class Public::AddressesController < ApplicationController
   before_action :authenticate_customer! #index
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @addresses = Address.all
+    if @address.save
+      redirect_to addresses_path
+    else
+      render :index
+    end
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(version: 2023_12_16_110644) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
+  
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
