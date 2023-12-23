05d69a41e6d0251e5d4cc7a567eaf47a7650888d
hiyohichi <hiyohichi07@gmail.com>
2023-12-23 04:50:18 +0000

    管理者側注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,25 @@
 class Admin::OrdersController < ApplicationController
+  
   def show
+    @order = Order.find(params[:id])
+    @selected_status = @order.status
+    @order_detail = @order.order_details
   end
+  
   def update
+    @order = Order.find(parems[:id])
+    @order_details = @order.ordere_details
+    @order.status = params[:status]
+    @order.update(order_params)
+      if @order.status == "confirmation"
+        @order_details.update_all(making_status: "mati")
+      end
+      redirect_to request.referer
   end
+  
+  private
+  def order_params
+    params.require(:order).permit(:status)
+  end
+  
 end
