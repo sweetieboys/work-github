87f50369bd4ea8fbec8c5737a9407fabbf4826ae
sakamotoyuuya <149666847+sakamotoyuuya@users.noreply.github.com>
2023-12-24 07:13:23 +0000

    orders finish

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,6 @@
 class OrderDetail < ApplicationRecord
-  belongs_to :orders
-  belongs_to :items
+  belongs_to :order
+  belongs_to :item
 
   #enumの定義　製造ステータス
   enum making_status: { fuka: 0, mati: 1, making: 2, complete: 3 }
