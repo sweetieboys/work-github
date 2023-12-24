103b573212934e91fab03333c627a7e3ad7f7784
sakamotoyuuya <149666847+sakamotoyuuya@users.noreply.github.com>
2023-12-24 12:10:46 +0000

    finish

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,23 +1,22 @@
 class Admin::OrderDetailsController < ApplicationController
-  
+
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
     @order_details = @order.order_details
-    @order_detail.making_status = params
     @order_detail.update(order_detail_params)
     if @order_details.all? { |detail| detail.making_status == "complete" } #全てのorderdetailのmaking_statusが制作完了の時
       @order.update(status: "preparing")                                   #orderのstatusを発送準備中に更新
     end
-    if @order_detail.making_status == "creationg" #orderdetailのmaking_statusが製作中の時
+    if @order_detail.making_status == "making" #orderdetailのmaking_statusが製作中の時
       @order.update(status: "creationg")          #orderのstatusを製作中に更新
     end
-    redirecto_to request.referer
+    redirect_to request.referer
   end
-  
+
   private
   def order_detail_params
     params.require(:order_detail).permit(:making_status)
   end
-    
+
 end
